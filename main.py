b56fa354d2f0efac44d274694b911627aaaccb2e
Lukydnomo <andrioboffpv@gmail.com>
2024-12-15 19:37:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -26,29 +26,33 @@ async def punir(ctx, member: discord.Member, punishChannel: discord.VoiceChannel
     # Verifica se o autor tem um cargo superior ao do bot
     if author_top_role.position <= bot_top_role.position and member_top_role.position:
         await ctx.send("Você precisa ter um cargo superior ao meu para usar este comando!")
-        
-    # Verifica se o autor está em um canal de voz
+    else:
         author_channel = ctx.author.voice.channel if ctx.author.voice else None
         if not author_channel:
             await ctx.send("Você não está em um canal de voz!")
+        else:
             try:
                 # Desabilita a permissão de conectar a outros canais de voz
                 for channel in ctx.guild.voice_channels:
                     if channel != punishChannel:
                         await channel.set_permissions(member, connect=False)
+
                 # Move o membro para o punishChannel
                 await member.move_to(punishChannel)
                 await ctx.send(f'{member.mention} foi punido e movido para {punishChannel.name}')
+
                 # Espera 60 segundos
                 await asyncio.sleep(60)
+
                 # Restaura as permissões para que o membro possa conectar aos outros canais de voz
                 for channel in ctx.guild.voice_channels:
                     if channel != punishChannel:
                         await channel.set_permissions(member, connect=True)
+
                 # Move o membro de volta para o canal original
                 await member.move_to(author_channel)
                 await ctx.send(f'{member.mention} foi movido de volta para {author_channel.name}')
+
             except Exception as e:
                 await ctx.send(f'Erro ao punir: {str(e)}')
-
 bot.run(TOKEN)
