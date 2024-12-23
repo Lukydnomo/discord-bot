31e0f90fd60abd986b260272f8a3b0594bdbe4f2
Lukydnomo <andrioboffpv@gmail.com>
2024-12-23 08:44:17 -0300

    upd

--- salvar_variavel.py
+++ salvar_variavel.py
@@ -1,18 +1,11 @@
-import json
 import sys
+import json
 
-def salvar_variavel(valor):
-    with open("sessaoclosedopen.json", "w") as file:
-        json.dump({"sessaoclosedopen": valor}, file)
+# Carregar o valor passado pelo argumento
+sessaoclosedopen = int(sys.argv[1])  # O valor que o bot passa
 
-if __name__ == "__main__":
-    # Captura o valor da variável a partir do argumento do comando
-    if len(sys.argv) > 1:
-        try:
-            valor = int(sys.argv[1])
-            salvar_variavel(valor)
-            print(f"Variável sessaoclosedopen salva com valor: {valor}")
-        except ValueError:
-            print("Erro: O valor fornecido não é um número válido.")
-    else:
-        print("Erro: Nenhum valor fornecido para salvar a variável.")
+# Salvar no arquivo JSON
+with open('sessaoclosedopen.json', 'w') as file:
+    json.dump({'sessaoclosedopen': sessaoclosedopen}, file)
+
+print(f"Valor de sessaoclosedopen salvo: {sessaoclosedopen}")
