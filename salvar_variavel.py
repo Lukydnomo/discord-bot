ac8a2d1569351d8f74ac7f4c200c8131cc27d5a3
Lukydnomo <andrioboffpv@gmail.com>
2024-12-23 09:19:33 -0300

    upd

--- salvar_variavel.py
+++ salvar_variavel.py
@@ -4,7 +4,7 @@ import json
 # Lê o valor da variável de ambiente
 sessaoclosedopen = int(os.getenv("sessaoclosedopen"))
 
-# Salvar no arquivo JSON
+# Salva no arquivo JSON
 with open('sessaoclosedopen.json', 'w') as file:
     json.dump({'sessaoclosedopen': sessaoclosedopen}, file)
 
