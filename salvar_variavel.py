5d89e438c3561c56f8547fdd697731bd607e4342
Lukydnomo <andrioboffpv@gmail.com>
2024-12-23 09:22:11 -0300

    upd

--- salvar_variavel.py
+++ salvar_variavel.py
@@ -2,7 +2,17 @@ import os
 import json
 
 # Lê o valor da variável de ambiente
-sessaoclosedopen = int(os.getenv("sessaoclosedopen"))
+sessaoclosedopen = os.getenv("sessaoclosedopen")
+
+if sessaoclosedopen is None or sessaoclosedopen == '':
+    print("Erro: sessaoclosedopen não foi definido corretamente!")
+    exit(1)
+
+try:
+    sessaoclosedopen = int(sessaoclosedopen)  # Converte para inteiro
+except ValueError:
+    print(f"Erro: não foi possível converter o valor '{sessaoclosedopen}' para inteiro.")
+    exit(1)
 
 # Salva no arquivo JSON
 with open('sessaoclosedopen.json', 'w') as file:
