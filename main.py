553c4bc493336374b447389a3a858f96c91d4760
Lukydnomo <andrioboffpv@gmail.com>
2025-03-22 14:56:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -328,9 +328,9 @@ async def on_message(message):
         
     # Respostas sarcasticas
     if len(message.content) > 150 and not is_spam(message.content):
-        await message.channel.trigger_typing()
-        await asyncio.sleep(3)
-        await message.channel.send(random.choice(SARCASM_RESPONSES))
+        async with message.channel.typing():  # Usa o contexto assíncrono para simular digitação
+            await asyncio.sleep(3)  # Aguarda 3 segundos (opcional)
+            await message.channel.send(random.choice(SARCASM_RESPONSES))  # Envia a resposta
 
     await bot.process_commands(message)
 
