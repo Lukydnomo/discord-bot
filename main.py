008c86b5081a306b3131329954184a40f6a3e337
Lukydnomo <andrioboffpv@gmail.com>
2025-03-27 00:51:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -602,7 +602,7 @@ async def on_message(message):
 
     # Palavras proíbidas (memes poggers heinn)
     if palavra_do_dia in message.content.lower():
-        await message.channel.send(f'{palavra_do_dia.upper} DETECTAD!!!! INICIANDO PROTOCOLO DE SEGURANÇA!!!!!')
+        await message.channel.send(f'{palavra_do_dia.upper()} DETECTAD!!!! INICIANDO PROTOCOLO DE SEGURANÇA!!!!!')
         await castigar_automatico(message.author, 60)
 
     await bot.process_commands(message)
